59435932c228aa68ae1f9fa48c3cf4530f6cef59
dalhaerthe <exte@interia.pl>
2025-04-23 20:02:53 +0200

    first

---
+++ app/src/main/java/a/rav/donat/MainActivity.kt
@@ -0,0 +1,36 @@
+package a.rav.donat
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Date
+
+class MainActivity : AppCompatActivity() {
+
+
+    lateinit var donka:Date
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        //odczyt daty donacji:
+
+        fun ReadDonDate(): Date{
+            //TODO
+        return Date(2025,1,22)
+        }
+    }
+}
